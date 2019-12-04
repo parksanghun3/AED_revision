69e8dac53a6276ca89d784cde82e664b364147b8
박상훈 <merrylifes@gmail.com>
2019-12-04 20:16:43 +0900

    est sample delay 크게 벗어났을 때, 아예 Nu 카운트에서 제외

--- AED_for_revision.m
+++ AED_for_revision.m
@@ -5,52 +5,85 @@ clear all; close all ; clc;
 C= 343;
 micdist = 0.12;
 M = 512; % length of impulse response vectors
-% mu = 0.003;
 mu = 0.001;
 % s = audioread('m60_68_FKFB0_con.wav');
 % s = audioread('m45_68_MDAC0_con.wav');
 
-RT60 = 0.6; %0.2 : 0.2 : 0.6;
-azimuth =-60; %-60:30:60;
+SNR = 0;
+RT60 = 0.4; %0.2 : 0.2 : 0.6;
+azimuth = -60 : 30 : 60 ;
 target = [{'female1'},{'female2'},{'female3'},{'male1'},{'male2'},{'male3'}];
-k = 4; %1:6;
-% load_dir = './1source/RT60_';
-[s,fs] = audioread(['./1source/RT60_' , num2str(RT60),'/',target{k},'/mixture_1x2_SNR_0_',num2str(azimuth),'도/x_1x2.wav']);
+Nu = 0; % utterence 개수
+Nu_out = 0; % est_sample_delay가 max_delay보다 큰 경우는 Nu에서 제외함.
 
+for target_index = 1 : 6
+    for azimuth_index = 1 : 5
+        
+        [s,fs] = audioread(['./1source/RT60_' , num2str(RT60),'/',target{target_index},'/mixture_1x2_SNR_0_',num2str(azimuth(azimuth_index)),'도/x_1x2.wav']);
+        
+        x1 = s(:,1);
+        x2 = s(:,2);
+        
+        % 초기화
+        g2 = [ zeros(1,M/2-1) 1 zeros(1,M/2)];
+        g1 = [0 zeros(1,M/2-1) zeros(1,M/2)];
+        
+        % u 업데이트
+        u = [g1 g2]';
+        len = floor((length(x1)-M)/M);
+        
+        for n = 1 : 1000000
+            
+            if (mod(n,len))~=0
+                i = mod(n,len);
+            else
+                i = len;
+            end
+            
+            x_temp(1,:) = x1((i*M) : -1 : (i-1)*M+1);
+            x_temp(2,:) = x2((i*M) : -1 : (i-1)*M+1);
+            x = [x_temp(2,:) -x_temp(1,:)]'; % 논문에서 x1*g2 = x2*g1 을 수행하기 위해, g는 그대로 두고 x의 좌우를 change
+            
+            e = u' * x;
+            u_temp = u - mu*e*x;
+            u = u_temp / norm(u_temp,2);
+            
+            %             if (mod(n,10000)==0)
+            %                 disp(['iteration = ' , num2str(n)])
+            %             end
 
-x1 = s(:,1);
-x2 = s(:,2);
-
-
-% 초기화
-g2 = [ zeros(1,M/2-1) 1 zeros(1,M/2)];
-g1 = [0 zeros(1,M/2-1) zeros(1,M/2)];
-
-% u 업데이트
-u = [g1 g2]';
-len = floor((length(x1)-M)/M);
-
-for n = 1 : 1000000
-
-    if (mod(n,len))~=0
-        i = mod(n,len);
-    else
-        i = len;
+        end
+        [a, b]=max(u(1:M));
+        [c, d]=max(u(M+1:2*M));
+        
+        max_delay = micdist*fs/C;
+        est_sample_delay = -(d-b);
+        
+        if abs(est_sample_delay) >= max_delay
+            est_sample_delay = true_sample_delay; % est sample delay가 max_delay보다 클 경우, MAE 계산시 제외하기 위해 est = true를 같게하여 diff를 0으로 만들어주고
+            Nu_out = Nu_out + 1; % MAE 식에서 Nu의 카운트를 다운시킴.
+        end
+        
+        true_sample_delay = micdist*sin(azimuth(azimuth_index)*pi/180)*fs / C;
+        est_azimuth = (asin(est_sample_delay * C /fs / micdist))*180/pi;
+        true_azimuth = (asin(true_sample_delay * C /fs / micdist))*180/pi;
+        Nu = Nu + 1;
+        diff_sample_delay(Nu,:) = abs(est_sample_delay - true_sample_delay);
+        
+        
+        disp(' ')
+        disp(['현재 음원 = RT60(' , num2str(RT60), ')_', target{target_index} ,'_azimuth(', num2str(azimuth(azimuth_index)), ')_' , 'SNR(',num2str(SNR),')'])
+%         disp(['현재 음원 = azimuth(', num2str(azimuth(azimuth_index)), ')_', target{target_index}, '_RT60(' , num2str(RT60),')_SNR(',num2str(SNR),')'])
+        disp(['True azimuth = ' ,num2str(true_azimuth), '˚']);
+        disp(['Est azimuth = ' ,num2str(est_azimuth), '˚']);
+        disp(['True Sample delay = ' ,num2str(true_sample_delay)]);
+        disp(['Est Sample delay = ' ,num2str(est_sample_delay)]);
+            
     end
-    
-    x_temp(1,:) = x1((i*M) : -1 : (i-1)*M+1);
-    x_temp(2,:) = x2((i*M) : -1 : (i-1)*M+1);
-    x = [x_temp(2,:) -x_temp(1,:)]'; % 논문에서 x1*g2 = x2*g1 을 수행하기 위해, g는 그대로 두고 x의 좌우를 change
-
-    e = u' * x;
-    u_temp = u - mu*e*x;
-    u = u_temp / norm(u_temp,2);
-   
-    if (mod(n,10000)==0)
-    disp(['iteration = ' , num2str(n)])
-    end
-    
 end
+
+Nu = Nu - Nu_out;
+MAE = sum(diff_sample_delay) / Nu;
 
 % for n = 1 : 5000000
 % 
@@ -78,22 +111,7 @@ end
 
 %  u = [g2 -g1]';
  
-[a, b]=max(u(1:M));
-[c, d]=max(u(M+1:2*M));
-
-max_delay = micdist*fs/C;
-est_sample_delay = -(d-b);
-true_sample_delay = micdist*sin(azimuth*pi/180)*fs / C;
-est_azimuth = (asin(est_sample_delay * C /fs / micdist))*180/pi;
-true_azimuth = (asin(true_sample_delay * C /fs / micdist))*180/pi;
-
-
-disp(['Max delay = ' ,num2str(max_delay)]);
-disp(['True azimuth = ' ,num2str(true_azimuth), '˚']);
-disp(['Est azimuth = ' ,num2str(est_azimuth), '˚']);
-disp(['True Sample delay = ' ,num2str(true_sample_delay)]);
-disp(['Est Sample delay = ' ,num2str(est_sample_delay)]);
-
+% disp(['Max delay = ' ,num2str(max_delay)]);
 
 figure(1)
 subplot(211)
